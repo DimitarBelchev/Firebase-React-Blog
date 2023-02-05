615c54d9e9ccca6d0863cafd6681d2e980fbeb8d
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-02-06 01:18:09 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import "./style.scss";
-import "./media-query.css";
 import Home from "./pages/Home/Home";
 import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
